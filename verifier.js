8e139881fc90003aff890b554fd587a43baf55f5
Anna Georgiadou <77627644+Annageorg@users.noreply.github.com>
2021-03-04 11:07:07 +0000

    size instead resolution

--- verifier.js
+++ verifier.js
@@ -1,17 +1,17 @@
 function verify(input) {
     
-    const resolution = 3;
+    const size = 3;
     //count neighbours function
     function getNeighbourCount(x, y) {
         let count = 0;
         for (let yy = -1; yy < 2; yy++) {
             for (let xx = -1; xx < 2; xx++) {
                 if (xx === 0 && yy === 0) continue;
-                if (x + xx < 0 || x + xx > resolution - 1) continue;
-                if (y + yy < 0 || y + yy > resolution - 1) continue;
+                if (x + xx < 0 || x + xx > size - 1) continue;
+                if (y + yy < 0 || y + yy > size - 1) continue;
                 if (input[x + xx][y + yy] === 1) count++;
-            }
-        }
+            };
+        };
         return count;
     }
 
@@ -22,8 +22,8 @@ function verify(input) {
             [1, 1, 1],
             [1, 1, 1],
         ];
-        for (let y = 0; y < resolution; y++) {
-            for (let x = 0; x < resolution; x++) {
+        for (let y = 0; y < size; y++) {
+            for (let x = 0; x < size; x++) {
                 const neighbours = getNeighbourCount(x, y);
                 if (input[x][y] == 1 && neighbours < 2) newArr[x][y] = 0;
                 if (input[x][y] == 1 && neighbours > 3) newArr[x][y] = 0;              
